f04507f93851bc8bd619142c05dd36c01fbefad5
AliceNotFromWonderland <alisa22112004@gmail.com>
2024-09-25 18:14:19 +0700

    Добавлены новые объекты TaxedIncome и OrganizationIncome

---
+++ pis1/TaxedIncome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis1
+{
+    internal class TaxedIncome
+    {
+    }
+}
