8ae39cdaa33b4663b33967ba3e8ee7477abbaf4a
AliceNotFromWonderland <alisa22112004@gmail.com>
2024-09-25 18:05:41 +0700

    Добавлена базовая версия проекта

---
+++ pis1/Income.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace pis1
+{
+    internal class Income
+    {
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+        public int Amount { get; set; }
+
+        public Income(DateTime date, string source, int amount)
+        {
+            Date = date;
+            Source = source;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Дата: {Date:yyyy.MM.dd}, Источник: {Source}, Сумма: {Amount}";
+        }
+
+        public static Income StrToIncome(string input)
+        {
+            string pattern = @"([0-9]{4}\.[0-9]{2}\.[0-9]{2})\s+\""(.*?)\""\s+(\d+)";
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                DateTime date = DateTime.ParseExact(match.Groups[1].Value, "yyyy.MM.dd", CultureInfo.InvariantCulture);
+                string source = match.Groups[2].Value;
+                int amount = int.Parse(match.Groups[3].Value);
+
+                return new Income(date, source, amount);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ pis1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "2023.09.24 \"Ежемесячная стипендия\" 100000000";
+            Income res = Income.StrToIncome(input);
+            Console.WriteLine(res.ToString());
+            Console.ReadKey();
+        }
+    }
+}
